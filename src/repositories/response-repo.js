af0d5ebcfebe3e6947e42f0f41f9884851b24858
Taiwo Adejare <adejareemma@gmail.com>
2022-01-20 21:19:17 +0100

    get all response

--- src/repositories/response-repo.js
+++ src/repositories/response-repo.js
@@ -1,8 +1,13 @@
-const Response = require("../models/response-model")
+const Response = require("../models/response-model");
 
 module.exports = {
      createResponse: async (newResponse) => {
         const response = await Response.create(newResponse);
         return response;
+    },
+
+    getAllResponse: async () => {
+        const responses = await Response.find();
+        return responses;
     }
 }
