17225ff734b63ed9350609c75a25da7f44f43316
Taiwo Adejare <adejareemma@gmail.com>
2022-01-20 21:19:29 +0100

    route to get user response

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 'use stict'
 
-const { notFound, sayHello, message } = require('../controllers/bot-controller');
+const { notFound, sayHello, message, getUserResponse } = require('../controllers/bot-controller');
 
 const router = require('express').Router();
 
 router.post('/', sayHello);
 router.post('/messages', message);
+router.get('/responses', getUserResponse);
 router.use('*', notFound);
 
 module.exports = router;
